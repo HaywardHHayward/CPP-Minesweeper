d4e5b77fd337e387068902f3902a8d56fd8b3224
Hayden Reckward <tictanic8@hotmail.com>
2024-08-05 16:47:10 -0500

    Decluttered main

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,12 @@
 #include "Tile.hpp"
 
 int main() {
+    using Minesweeper::Board, Minesweeper::BoardComponentBase, Minesweeper::BoardComponent;
     int num;
     std::cin >> num;
-    Minesweeper::Board board{static_cast<std::uint8_t>(num), 5, 5};
+    Board board{static_cast<std::uint8_t>(num), 5, 5};
     ftxui::ScreenInteractive screen{ftxui::ScreenInteractive::Fullscreen()};
-    const Minesweeper::BoardComponent boardComponent{
-        Minesweeper::BoardComponentBase::Create(board, screen.ExitLoopClosure())};
+    const BoardComponent boardComponent{BoardComponentBase::Create(board, screen.ExitLoopClosure())};
     screen.Loop(Hoverable(boardComponent, boardComponent->hovered()) | ftxui::center | ftxui::flex);
     std::cin.peek();
     return 0;
