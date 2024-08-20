9a6e0b3029281dfbd026d13a9ee56d09474c83b8
HaywardHHayward <tictanic8@hotmail.com>
2024-08-20 12:43:36 -0500

    Refactored TCB->Render() to reduce nesting

--- src/TileComponentBase.cpp
+++ src/TileComponentBase.cpp
@@ -19,53 +19,49 @@ namespace Minesweeper {
                                                                m_hovered{false} { }
 
     ftxui::Element TileComponentBase::Render() {
-        ftxui::Element tileRepr;
+        using namespace ftxui;
         if (!m_tile.isChecked()) {
-            tileRepr = m_tile.isFlagged()
-                           ? ftxui::text("P") | color(ftxui::Color::Red)
-                           : ftxui::text(" ") | color(ftxui::Color());
-            tileRepr |= bgcolor(ftxui::Color::GrayLight);
-            return tileRepr;
+            if (m_tile.isFlagged()) {
+                return text("P") | color(Color::Red) | bgcolor(Color::GrayLight);
+            }
+            return text(" ") | bgcolor(Color::GrayLight);
         }
         if (m_tile.isMine()) {
-            tileRepr = ftxui::text("*") | color(ftxui::Color::Black);
-        } else {
-            tileRepr = ftxui::text(m_tile.getSurroundingMines() == 0
-                                       ? " "
-                                       : std::to_string(m_tile.getSurroundingMines()));
-            switch (m_tile.getSurroundingMines()) {
-                [[likely]] case 0:
-                    tileRepr |= color(ftxui::Color());
-                    break;
-                [[likely]] case 1:
-                    tileRepr |= color(ftxui::Color::Blue);
-                    break;
-                [[likely]] case 2:
-                    tileRepr |= color(ftxui::Color::Green);
-                    break;
-                [[likely]] case 3:
-                    tileRepr |= color(ftxui::Color::Red);
-                    break;
-                case 4:
-                    tileRepr |= color(ftxui::Color::DarkBlue);
-                    break;
-                case 5:
-                    tileRepr |= color(ftxui::Color::DarkRed);
-                    break;
-                case 6:
-                    tileRepr |= color(ftxui::Color::Cyan);
-                    break;
-                [[unlikely]] case 7:
-                    tileRepr |= color(ftxui::Color::Black);
-                    break;
-                [[unlikely]] case 8:
-                    tileRepr |= color(ftxui::Color::GrayLight);
-                    break;
-                default: // this should never be reached since it'll be a violation of Tile's surroundingMines invariant
-                    UNREACHABLE();
-            }
+            return text("*") | color(Color::Black) | bgcolor(Color::GrayDark);
         }
-        tileRepr |= bgcolor(ftxui::Color::GrayDark);
+        const uint8_t surroundingMines = m_tile.getSurroundingMines();
+        if(surroundingMines == 0) [[likely]] {
+            return text(" ") | bgcolor(Color::GrayDark);
+        }
+        Element tileRepr{text(std::to_string(surroundingMines)) | bgcolor(Color::GrayDark)};
+        switch (surroundingMines) {
+            [[likely]] case 1:
+                tileRepr |= color(Color::Blue);
+                break;
+            [[likely]] case 2:
+                tileRepr |= color(Color::Green);
+                break;
+            [[likely]] case 3:
+                tileRepr |= color(Color::Red);
+                break;
+            case 4:
+                tileRepr |= color(Color::DarkBlue);
+                break;
+            case 5:
+                tileRepr |= color(Color::DarkRed);
+                break;
+            case 6:
+                tileRepr |= color(Color::Cyan);
+                break;
+            [[unlikely]] case 7:
+                tileRepr |= color(Color::Black);
+                break;
+            [[unlikely]] case 8:
+                tileRepr |= color(Color::GrayLight);
+                break;
+            default: // this should never be reached since it'll be a violation of Tile's surroundingMines invariant
+                UNREACHABLE();
+        }
         return tileRepr;
     }
 
